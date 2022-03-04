5d540d7ce9b8e86ff3bd11945102a47dc1a21412
Pumpkin <1257090532@qq.com>
2022-03-04 19:44:28 +0800

    Convert Roman numerals to integers

---
+++ src/罗马数字转整数/romanToIntTest.java
@@ -0,0 +1,13 @@
+package 罗马数字转整数;
+
+/**
+ * @Author: Pumpkin
+ * @Date: 2022/3/4
+ */
+public class romanToIntTest {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int romanToInt = solution.romanToInt("MCMXCIV");
+        System.out.println(romanToInt);
+    }
+}
