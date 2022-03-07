6a311f9e7ae0d5961b4ee4be43111fdbc83b69f2
Pumpkin <1257090532@qq.com>
2022-03-07 16:54:03 +0800

    convertToBase7Test

---
+++ src/七进制数/Solution.java
@@ -0,0 +1,11 @@
+package 七进制数;
+
+/**
+ * @Author: Pumpkin
+ * @Date: 2022/3/7
+ */
+public class Solution {
+    public String convertToBase7(int num) {
+        return Integer.toString(num,7);
+    }
+}
---
+++ src/七进制数/convertToBase7Test.java
@@ -0,0 +1,13 @@
+package 七进制数;
+
+/**
+ * @Author: Pumpkin
+ * @Date: 2022/3/7
+ */
+public class convertToBase7Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String convertToBase7 = solution.convertToBase7(20);
+        System.out.println(convertToBase7);
+    }
+}
