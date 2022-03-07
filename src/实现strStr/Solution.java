b4f1b95982de6b7a47f83c29af4977ca785a5377
Pumpkin <1257090532@qq.com>
2022-03-07 17:47:02 +0800

    strStr

---
+++ src/实现strStr/Solution.java
@@ -0,0 +1,11 @@
+package 实现strStr;
+
+/**
+ * @Author: Pumpkin
+ * @Date: 2022/3/7
+ */
+public class Solution {
+    public int strStr(String haystack, String needle){
+        return haystack.indexOf(needle);
+    }
+}
---
+++ src/实现strStr/strStrTest.java
@@ -0,0 +1,13 @@
+package 实现strStr;
+
+/**
+ * @Author: Pumpkin
+ * @Date: 2022/3/7
+ */
+public class strStrTest {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int strStr = solution.strStr("hello", "ll");
+        System.out.println(strStr);
+    }
+}
