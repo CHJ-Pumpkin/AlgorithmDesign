4f65d5f162e54bf25459aee711f25ca240e454d9
Pumpkin <1257090532@qq.com>
2022-03-07 18:34:35 +0800

    addBinary

---
+++ src/二进制求和/addBinaryTest.java
@@ -0,0 +1,13 @@
+package 二进制求和;
+
+/**
+ * @Author: Pumpkin
+ * @Date: 2022/3/7
+ */
+public class addBinaryTest {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String addBinary = solution.addBinary("11", "1");
+        System.out.println(addBinary);
+    }
+}
